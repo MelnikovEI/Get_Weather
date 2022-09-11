3e42ba28f8a106d79b228d9197315546dfb34c4f
EvgenyM <MelnikovEI@ya.ru>
2022-09-11 21:13:56 +0300

    Added errors protection in GET request

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import requests
 
 
-try:
-    city_number = int(input("Введите цифру, соответствующую городу, в котором хотите узнать прогноз погоды:"
-                            "1 - Лондон, 2 - Шереметьево, 3 - Череповец\n"))
-except ValueError:
-    print('Ввод не похож на цифру 1, 2, или 3 :)')
-    exit(-1)
-if city_number not in (1, 2, 3):
-    raise ValueError('Введена не корректная цифра')
-cities = {1: 'Лондон', 2: 'Шереметьево', 3: 'Череповец'}
-url_template = 'http://www.wttr.in/{}?nTqmM&lang=ru%20HTTP/1.1'
-url = url_template.format(cities[city_number])
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
+params = {
+    'nTqmM': '',
+    'lang': 'ru',
+    ' HTTP/1.1': ''
+}
+url_template = 'http://www.wttr.in/{}'
+cities = ('Лондон', 'Шереметьево', 'Череповец')
+for city in cities:
+    url = url_template.format(city)
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    print(response.text)
